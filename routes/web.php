d7842b67ab381d19fd5d38e1a601260d2385d5c6
walid <mohamad.walid@spil.co.id>
2025-05-14 15:38:55 +0700

    --f: navigate bar

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,10 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 // === ADMIN PANEL ===
-Route::prefix('admin')->middleware(['auth', 'role:admin'])->group(function () {
+Route::middleware(['auth', 'role:admin'])->prefix('admin')->group(function () {
     Route::name('admin.')->group(function () {
         Route::get('/', function () {
-            dd('asd');
-            // return view('admin.dashboard');
+            return view('admin.dashboard');
         })->name('index');
 
         Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
