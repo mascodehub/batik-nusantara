c8545ad391abe96baafd293de8f4d068d8d542e2
wabot <walidshr8@gmail.com>
2025-05-14 22:51:39 +0700

    --a: navigasi asset

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -34,9 +34,9 @@
                     </x-slot>
 
                     <x-slot name="content">
-                        {{-- <x-dropdown-link :href="route('admin.profile.edit')">
+                        <x-dropdown-link :href="route('admin.profile.edit')">
                             {{ __('Profile') }}
-                        </x-dropdown-link> --}}
+                        </x-dropdown-link>
 
                         <!-- Authentication -->
                         <form method="POST" action="{{ route('admin.logout') }}">
